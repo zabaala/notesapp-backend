e79beb37bc35e8d666dffa837f880ff8b947c5d8
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2020-02-10 22:55:19 +0000

    initial commit

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use \App\Domains\Notes\Note;
+use Faker\Generator as Faker;
+
+$factory->define(Note::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(124),
+        'text' => $faker->paragraph
+    ];
+});
